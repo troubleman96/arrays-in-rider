49226d149bf76371c861ce2d25e4ca2f057be252
troubleman96 <itslugenge96@gmail.com>
2024-11-23 00:17:37 +0300

    no1

---
+++ Arrrayys/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            int no1 = 5;
+            int no2 = 10;
+            int no3 = 15;
+
+            int[] numbers = new int[3];
+
+            Console.WriteLine("Enter ur first number: ");
+            numbers[0] = Convert.ToInt32(Console.ReadLine()); 
+            
+            Console.WriteLine("Enter ur second number: ");
+            numbers[1] = Convert.ToInt32(Console.ReadLine()); 
+            
+            Console.WriteLine("Enter ur third number: ");
+            numbers[2] = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine($"{no1}, {no2}, {no3}");
+            Console.WriteLine($"{numbers[0]}, {numbers[1]}, {numbers[2]}");
+        }
+    }
+}
